3b928545b761f8d0cfdf4c055c1630dfbdd94f69
Mahmudur Rahman <nahidacm@gmail.com>
2025-02-09 09:47:51 +0600

    epic backup update fix

--- commands/update-epic-backup.js
+++ commands/update-epic-backup.js
@@ -54,6 +54,14 @@ export const updateEpicBackupCommand = async (options) => {
             fs.copyFileSync(db_file, backup_db_file);
         }
 
+        // Backup uploaded files
+        const uploads_dir = path.join(targetDir, currentVersion, 'epic-backup-api', 'files');
+        const backup_uploads_dir = path.join(targetDir, 'temp', 'files');
+        if (fs.existsSync(uploads_dir)) {
+            fs.mkdirSync(backup_uploads_dir, { recursive: true });
+            fs.copySync(uploads_dir, backup_uploads_dir);
+        }
+
         // Update
         await performEpicBackupUpdate(answers);
 
